c92da2135e90bea44e2eab39fac2b5d6d9031159
Alexandru Mihai Ion <ionmihai09@gmail.com>
2016-03-04 00:02:16 +0000

    Caffeine App, but using the map.
    whole project

---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('caffeinehit', [
+	'ionic',
+  'ngMap',
+  'ngCordova',
+	'caffeinehit.controllers',
+	'caffeinehit.services',
+	'caffeinehit.filters'
+]);
+
+app.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
+
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+});
+
+app.config(function ($httpProvider) {
+	$httpProvider.defaults.headers.common['Authorization'] = 'Token c28b812efe6448fb10621cb89bd7f076e328ebb6';
+});
